4512ec9a36d6a6316a5e907eaceace400e1b752b
mehdibehboudi2002 <mehdi061280@gmail.com>
2024-10-21 15:59:30 +0330

    add generateStaticParams for resolve build error

--- my-app/app/products/[category]/page.tsx
+++ my-app/app/products/[category]/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 import ShopProducts from "@/components/Shop/products";
+import { SHOP_CATEGORIES } from "@/constants";
+
+export async function generateStaticParams() {
+  const categories = SHOP_CATEGORIES.categories.map(cat => cat.category);
+
+  return categories.map(category => ({
+    category,
+  }));
+}
 
 const ShopProductsByCategory = () => {
-  return <ShopProducts/>
-}
+  return <ShopProducts />;
+};
 
 export default ShopProductsByCategory;
