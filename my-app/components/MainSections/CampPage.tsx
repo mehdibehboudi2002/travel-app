d26dca888e469d37e338c1ca8f26a5629a51eca8
mehdibehboudi2002 <mehdi061280@gmail.com>
2024-10-24 17:41:27 +0330

    updated map markers

--- my-app/components/MainSections/CampPage.tsx
+++ my-app/components/MainSections/CampPage.tsx
@@ -10,9 +10,9 @@ import GetApp from '@/components/MainSections/GetApp';
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
 });
 
 type CampType = {
